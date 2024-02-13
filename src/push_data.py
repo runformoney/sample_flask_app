65cc595d03c1742ea07a185351c4146e980b0fb8
Rukhshan.com <RukshanUrRahman@gmail.com>
2024-02-14 02:50:27 +0530

    1. Changing push_data.py code

--- src/push_data.py
+++ src/push_data.py
@@ -1,38 +1,35 @@
+import os
 from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-import logging
 
-logging.getLogger().setLevel(logging.INFO)
+# Replace 'postgresql://user:password@localhost:5432/database' with your actual PostgreSQL connection string
+DATABASE_URI = os.getenv("DATABASE_URI")
+engine = create_engine(DATABASE_URI)
 
+Base = declarative_base()
 
-def get_all_students():
-    # Replace 'postgresql://user:password@localhost:5432/database' with your actual PostgreSQL connection string
-    DATABASE_URI = os.getenv("DATABASE_URI")
-    engine = create_engine(DATABASE_URI)
+# Define the table class
+class Student(Base):
+    __tablename__ = 'students'
 
-    Base = declarative_base()
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    roll_number = Column(Integer)
 
-    # Define the table class
-    class Student(Base):
-        __tablename__ = "students"
+# Create the table in the database
+Base.metadata.create_all(engine)
 
-        id = Column(Integer, primary_key=True)
-        name = Column(String)
-        roll_number = Column(Integer)
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    # Create the table in the database
-    Base.metadata.create_all(engine)
+# Insert two rows
+student1 = Student(name="Rukhshan Ur Rehman", roll_number=3)
+# student2 = Student(name="Hifzur Rahman", roll_number=2)
 
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-    session = Session()
+session.add_all([student1])
+session.commit()
 
-    # Retrieve all rows from the 'students' table
-    all_students = session.query(Student).all()
-
-    # Close the session
-    session.close()
-    resp = {"data": all_students}
-    return resp
+# Close the session
+session.close()
